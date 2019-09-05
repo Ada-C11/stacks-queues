fec7fd9341abc1181dc725951eeb5c5d3daa08a9
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-09-05 16:11:45 -0700

    wrote enqueue method for tests to pass and also refactored to_s

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,12 +1,24 @@
 class Queue
+  QUEUE_SIZE = 20
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = [Array.new(QUEUE_SIZE)]
+    @front = @rear = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 #if front is empty
+      @rear = 1
+      @front = 0
+
+      @store[@front] = element
+    elsif @front == @rear
+      raise Error, "queue full!"
+    else
+      new_rear = (@rear + 1) % QUEUE_SIZE
+      @store[@rear] = element
+      @rear = new_rear
+    end
   end
 
   def dequeue
@@ -26,6 +38,8 @@ class Queue
   end
 
   def to_s
-    return @store.to_s
+    adjusted_store = @store.select {|val| val != nil}
+
+    return adjusted_store.to_s
   end
 end

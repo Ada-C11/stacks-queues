41bd2eb3a00997a4ee07398a559554d1864a2ce3
Kim Fasbender <v-kimberlyf@zillowgroup.com>
2019-09-05 16:11:59 -0700

    added functionality for enqueuing an element

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,12 +1,26 @@
-class Queue
+QUEUE_SIZE = 20
 
+class Queue
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(QUEUE_SIZE)
+    # starting as -1 because nothing is in the queue yet
+    @front = @rear = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      @front = 0
+      @rear = 1
+      @store[@front] = element
+    elsif @front == @rear
+      raise Error, "Q FULL"
+    else
+      new_rear = (@rear + 1) % QUEUE_SIZE # if we reach the end of the array it will wrap around to the beginning again...
+      @store[@rear] = element
+      @rear = new_rear
+    end
+
+    return @store
   end
 
   def dequeue
@@ -26,6 +40,7 @@ class Queue
   end
 
   def to_s
-    return @store.to_s
+    new_store = @store.select { |num| num != nil }
+    return new_store.to_s
   end
 end

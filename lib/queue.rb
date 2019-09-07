cf94f7a8c84f849fdc6c7634ebe711574e6785f5
Kirsten Anderson <kirsten.anderson@avalara.com>
2019-09-07 13:58:59 -0700

    All queue tests pass

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,61 @@
-class Queue
+QUEUE_SIZE = 20
 
+class Queue
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(QUEUE_SIZE)
+    @front = @rear = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      @rear = 1
+      @front = 0
+      @store[@front] = element
+    elsif @front == @rear
+      raise Error, "Queue full"
+    else
+      @store[@rear] = element
+      new_rear = (@rear + 1) % QUEUE_SIZE
+      @rear = new_rear
+    end
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    value = @store[@front]
+    if @front + 1 == @rear # list will be empty
+      @front = -1
+      @rear = -1
+    else
+      @front = (@front + 1) % QUEUE_SIZE
+    end
+    return value
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    if @front <= @rear
+      return @rear - @front
+    else
+      return (@store.length - @front) + @rear
+    end
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      return true
+    end
+    return false
   end
 
   def to_s
+    if @front < @rear
+      @store = @store[@front...@rear]
+    else
+      @store = @store[@front..-1].concat(@store[0...@rear])
+    end
     return @store.to_s
   end
 end

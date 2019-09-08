ccb78eda6dd66f18ed86e9dcc5e575248fbb5a4a
Kim Fasbender <v-kimberlyf@zillowgroup.com>
2019-09-07 21:23:19 -0700

    added functionality for checking if a string is balanced and passing all tests

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,26 @@
-require_relative './stack.rb'
+require_relative "./stack.rb"
+
+PAREN_PAIRS = {
+  "{" => "}",
+  "[" => "]",
+  "(" => ")",
+}
 
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+
+  return true if string == ""
+
+  string.each_char do |char|
+    if PAREN_PAIRS[char]
+      stack.push(char)
+    else
+      opening_paren = stack.pop
+      return false if char != PAREN_PAIRS[opening_paren]
+    end
+  end
+
+  return stack.empty?
 end
 
 def evaluate_postfix(postfix_expression)

d4397af177ca86e44730adcb27b3a979b8840040
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-09-07 18:30:07 -0700

    enqueue and queue.to_s

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,12 +1,24 @@
+QUEUE_SIZE = 20
+
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(QUEUE_SIZE)
+    @front = @rear = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 # Q is empty
+      @rear = 1
+      @front = 0
+      @store[@front] = element
+    elsif @front == @rear
+      raise Error, "Q IS FULL"
+    else #not empty
+      new_rear = (@rear + 1) % QUEUE_SIZE
+      @store[@rear] = element
+      @rear = new_rear
+    end
   end
 
   def dequeue
@@ -26,6 +38,13 @@ class Queue
   end
 
   def to_s
-    return @store.to_s
+    values = []
+    @store.each do |i|
+      if i != nil 
+        values << i
+      end
+    end
+
+    return values.to_s
   end
 end

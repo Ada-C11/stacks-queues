ce0b2e996f8bcc429f50c46d2fc8f9c7eb2d11de
Heather Izumi <hkizumi@gmail.com>
2019-09-08 12:49:32 -0700

    added pop and empty? methods

--- lib/stack.rb
+++ lib/stack.rb
@@ -3,27 +3,20 @@ require_relative "linked_list.rb"
 class Stack
   def initialize
     @store = LinkedList.new
-    @top = nil
+    # @top = nil
   end
 
   def push(element)
-    #If using a linked list for a stack, is it really necessary to use a doubly linked list? Could you just use the add_first and remove_first methods of a single linked list?
-    @top = @store.add_last(element)
+    #If using a linked list for a stack, is there a disadvantage to using a singly linked list and use the add_first method for push and remove_first method for pop?
+    @store.add_last(element)
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    @store.remove_last()
   end
 
   def empty?
-    # if @top.nil?
-    #   return true
-    # else
-    #   return false
-    # end
-
-    return true if @top.nil?
-    return false
+    return @store.empty?
   end
 
   def to_s

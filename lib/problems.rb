aa336044ba9801fb2577a24a70c4d7000b48c965
Heather Izumi <hkizumi@gmail.com>
2019-09-08 14:25:17 -0700

    Added balanced method to problems.rb

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,25 @@
-require_relative './stack.rb'
+require_relative "./stack.rb"
 
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return true if string == ""
+
+  parens_hash = {
+    "(" => ")",
+    "[" => "]",
+    "{" => "}",
+  }
+
+  stack_parens = Stack.new
+  string.each_char do |paren|
+    if parens_hash[paren]
+      stack_parens.push(parens_hash[paren])
+    else
+      return false if paren != stack_parens.pop
+    end
+    puts stack_parens.to_s
+  end
+
+  return stack_parens.empty?
 end
 
 def evaluate_postfix(postfix_expression)

d73a89da14e6675da2fff4bbe82c7cb65817b2f7
Kim Fasbender <v-kimberlyf@zillowgroup.com>
2019-09-07 23:34:22 -0700

    added peek and size functionality to the implementation of stack

--- lib/stack.rb
+++ lib/stack.rb
@@ -3,14 +3,24 @@ require_relative "./linked_list"
 class Stack
   def initialize
     @store = LinkedList.new
+    @top = nil
   end
 
   def push(element)
     @store.add_last(element)
+    @top = element
   end
 
   def pop
     @store.remove_last
+  end
+
+  def peek
+    return @top
+  end
+
+  def size
+    return @store.length
   end
 
   def empty?

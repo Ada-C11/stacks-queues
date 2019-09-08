7b9d5990b1c5de4d31debd2636d18cc265ac94ee
Jansen <v.jansen.martin@gmail.com>
2019-09-08 09:27:11 -0700

    Complete 'evaluate_postfix' function.  All tests passing

--- lib/problems.rb
+++ lib/problems.rb
@@ -4,6 +4,8 @@ PAREN = { ")": "(",
           "}": "{",
           "]": "[" }
 
+OPERATORS = ["+", "-", "*", "/"]
+
 def balanced(string)
   return true if string == ""
   return false if string.length % 2 != 0
@@ -31,5 +33,25 @@ def balanced(string)
 end
 
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+
+  i = 0
+  while i <= postfix_expression.length - 1
+    target = postfix_expression[i]
+
+    if !OPERATORS.include?(target)
+      stack.push(target.to_i)
+    else
+      operator = target
+      operand_second = stack.pop
+      operand_first = stack.pop
+
+      result = (operand_first).public_send(operator, operand_second)
+      stack.push(result)
+    end
+
+    i += 1
+  end
+
+  return stack.pop
 end

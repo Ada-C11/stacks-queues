db9a9c72e037c63c76c84c3d56587381088ec717
Jansen <v.jansen.martin@gmail.com>
2019-09-08 09:01:07 -0700

    Complete 'balanced' method.  All tests passing

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,37 @@
-require_relative './stack.rb'
+require_relative "./stack.rb"
+
+# PAREN = { "(": ")",
+#          "{": "}",
+#          "[": "]" }
+
+PAREN = { ")": "(",
+          "}": "{",
+          "]": "[" }
 
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return true if string == ""
+  return false if string.length % 2 != 0
+
+  stack = Stack.new
+  balanced = true
+
+  i = 0
+  while i <= string.length - 1
+    target = string[i]
+    balanced = true
+
+    if PAREN.value?(target)
+      stack.push(target)
+    else
+      popped = stack.pop
+      balanced = PAREN[target.to_sym] == popped
+    end
+
+    return false if !balanced
+    i += 1
+  end
+
+  return balanced
 end
 
 def evaluate_postfix(postfix_expression)

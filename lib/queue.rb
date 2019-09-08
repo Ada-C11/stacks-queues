a89a4a3fa4deb6495f5735ed7df934e14ba41af1
Kelly Downes <kedownes@gmail.com>
2019-09-08 15:01:10 -0700

    Implement queue methods

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,60 @@
-class Queue
+QUEUE_SIZE = 20
 
+class Queue
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(QUEUE_SIZE)
+    @front = @rear = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      @rear = 1
+      @front = 0
+      @store[@front] = element
+    elsif @front == @rear
+      raise Error, "Queue full"
+    else
+      new_rear = (@rear + 1) % QUEUE_SIZE
+      @store[@rear] = element
+      @rear = new_rear
+    end
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    removed = @store[@front]
+    if @front + 1 == @rear
+      @front = -1
+      @rear = -1
+    else
+      @store[@front] = nil
+      @front = (@front + 1) % QUEUE_SIZE
+    end
+    return removed
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    if @store.empty?
+      return -1
+    else
+      return @store[@front]
+    end
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @store.size
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front == -1
   end
 
   def to_s
-    return @store.to_s
+    store_string = []
+    @store.each do |num|
+      if num != nil
+        store_string << num
+      end
+    end
+    return store_string.to_s
   end
 end

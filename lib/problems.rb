db488d1f1307daf9e516a2341c2c05e76a1e3e22
TATIANA QUINTANA <tquintana@indeed.com>
2019-09-08 16:08:09 -0700

    added logic for balanced prob

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,26 @@
 require_relative './stack.rb'
 
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  left_stack = Stack.new
+
+  return false if string.length % 2 != 0 
+
+  i = 0
+  while i < string.length
+    if string[i] == '{' || string[i] == '(' || string[i] == '['
+      left_stack.push(string[i])
+    else
+      if left_stack.empty?
+        return false
+      end
+      if string[i] == ')' && left_stack.pop != '(' || string[i] == ']' && left_stack.pop != '[' || string[i] == '}' && left_stack.pop != '{'
+        return false
+      end
+    end
+    i += 1
+  end
+
+  return true
 end
 
 def evaluate_postfix(postfix_expression)

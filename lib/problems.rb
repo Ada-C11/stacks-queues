60c70a88546399912eeabf89368251ca91924a5c
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-09-08 12:03:24 -0700

    wrote balanced method to make tests pass

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,32 @@
-require_relative './stack.rb'
+require_relative "./stack.rb"
 
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return true if string.length == 0
+
+  stack = Stack.new
+
+  pairs = {
+    "(" => ")",
+    "{" => "}",
+    "[" => "]",
+  }
+
+  string.each_char do |char|
+    if pairs.include?(char) # it's an opening parens
+      stack.push(char)
+    else # it's a closing parens
+      last_value = stack.pop
+      if char != pairs[last_value]
+        return false
+      end
+    end
+  end
+
+  if stack.empty?
+    return true
+  else
+    return false
+  end
 end
 
 def evaluate_postfix(postfix_expression)

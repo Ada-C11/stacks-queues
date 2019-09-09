92bcd99dd156bc528d3aa66925020e65897c5ab3
Margaret Finnan <msfinnan@gmail.com>
2019-09-08 21:40:42 -0700

    balanced

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,23 @@
-require_relative './stack.rb'
+require_relative "./stack.rb"
 
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new()
+  string.each_char do |c|
+    if c == "(" || c == "[" || c == "{"
+      stack.push(c)
+    else
+      prev = stack.pop
+      if c == "}"
+        return false if prev != "{"
+      elsif c == ")"
+        return false if prev != "("
+      elsif c == "]"
+        return false if prev != "["
+      end
+    end
+  end
+  result = stack.empty?
+  return result
 end
 
 def evaluate_postfix(postfix_expression)

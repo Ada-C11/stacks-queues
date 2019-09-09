b0690229d579e078209b15586778b6710ec69e15
amyesh <amyesh08@gmail.com>
2019-09-08 20:30:54 -0700

    completed balanced method

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,23 @@
-require_relative './stack.rb'
+require_relative "./stack.rb"
 
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  parens_hash = {
+    "(" => ")",
+    "[" => "]",
+    "{" => "}",
+  }
+  stack = Stack.new
+  array = string.split("")
+  return false if array.length % 2 == 1
+  
+  array.each do |char|
+    if parens_hash.include?(char)
+      stack.push(char)
+    else
+      return false if char != parens_hash[stack.pop]
+    end
+  end
+  return true
 end
 
 def evaluate_postfix(postfix_expression)

184a23145be67db824f08f24e3f9d42c3ac50836
Erica Norris <norrise120@gmail.com>
2019-09-09 20:57:32 -0700

    added methods for stack and unskipped tests

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,22 +1,46 @@
 class Stack
+  STACK_SIZE = 20
+
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(QUEUE_SIZE)
+    @front = @rear = -1
   end
 
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      @rear = 1
+      @front = 0
+      @store[@front] = element
+    elsif @front == @rear
+      raise ArgumentError, "Stack is full"
+    else
+      new_rear = (@rear + 1) % STACK_SIZE
+      @store[@rear] = element
+      @rear = new_rear
+    end
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 
+      raise ArgumentError, "Stack is empty"
+    else
+      popped = @store[@rear - 1]
+      @store[@rear - 1] = nil
+      @rear = (@rear - 1) % STACK_SIZE
+      return popped
+    end
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    stack = @store.compact
+    if stack.length == 0
+      return true
+    else
+      return false
+    end
   end
 
   def to_s
-    return @store.to_s
+    return @store.compact.to_s
   end
 end

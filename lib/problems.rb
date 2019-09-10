54eea4c973872a2f8118dd8b70abd0cbe0ebb78a
sjscotton <sj.scotton@gmail.com>
2019-09-09 19:56:09 -0700

    finished stack, queue and balanced problems

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,16 @@
-require_relative './stack.rb'
+require_relative "./stack.rb"
 
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+  pairs = { ")" => "(", "]" => "[", "}" => "{" }
+  string.each_char do |bracket|
+    if pairs.values.include?(bracket)
+      stack.push(bracket)
+    else
+      return false if stack.empty? or pairs[bracket] != stack.pop()
+    end
+  end
+  return stack.empty?
 end
 
 def evaluate_postfix(postfix_expression)
--- lib/queue.rb
+++ lib/queue.rb
@@ -1,16 +1,33 @@
 class Queue
+  QUEUE_SIZE = 20
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    # @store = ..
+    @store = Array.new(QUEUE_SIZE)
+    @front = @rear = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 # Q is empty
+      @rear = 1
+      @front = 0
+      @store[@front] = element
+    elsif @front == @rear
+      raise Error, "Q Full"
+    else # not empty
+      new_rear = (@rear + 1) % QUEUE_SIZE
+      @store[@rear] = element
+      @rear = new_rear
+    end
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    raise Error, "Q Empty" if @front == -1
+    value = @store[@front]
+    @store[@front] = nil
+    @front = (@front + 1) % QUEUE_SIZE
+    @front = @rear = -1 if @front == @rear
+    return value
   end
 
   def front
@@ -22,10 +39,10 @@ class Queue
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @rear == -1
   end
 
   def to_s
-    return @store.to_s
+    return @store[@front...@rear].to_s
   end
 end

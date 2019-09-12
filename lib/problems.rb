692c437884cec2b6ce31fc9d9f5498aa90778b5f
Chu <ejpz@M-C02YN16UJG5J.nordstrom.net>
2019-09-12 12:56:26 -0700

    added notes to balanced

--- lib/problems.rb
+++ lib/problems.rb
@@ -2,6 +2,8 @@ require_relative "./stack.rb"
 
 def balanced(string)
   return true if string == ""
+
+  # set up
   stack = Stack.new
   char_map = {
     "}" => "{",
@@ -9,6 +11,12 @@ def balanced(string)
     ")" => "(",
   }
   open_chars = ["{", "[", "("]
+
+  # loop through each char
+  # push to stack if it's opening char. if not, look up corresponding value in hash
+  # compare hash value to popped stack node. if they are not equal or hash value is nil, return false
+  # finally, check if the stack is empty. if it is, it means everything matches up
+
   string.each_char do |c|
     if open_chars.include?(c)
       stack.push(c)

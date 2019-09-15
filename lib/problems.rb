b4013e8860d60bcffcc69b2607e0b8a3a703a79d
Amy Phung <phung.amy85@gmail.com>
2019-09-15 14:26:41 -0700

    complete exercises

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,27 @@
 require_relative './stack.rb'
 
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  bracket_buds = {
+    '(' => ')',
+    '[' => ']',
+    '{' => '}'
+  }
+
+  stack = Stack.new
+  list = string.split('')
+
+  if list.length % 2 != 0
+    return false
+  end
+  
+  list.each do |char|
+    if bracket_buds.include?(char)
+      stack.push(char)
+    elsif char != bracket_buds[stack.pop]
+      return false
+    end
+  end  
+    return true
 end
 
 def evaluate_postfix(postfix_expression)

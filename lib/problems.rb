f6665b78e5199df7300feef0f5145b2dbf56a80c
Shubha Rajan <shubharajan94@gmail.com>
2019-09-18 00:53:19 -0700

    balanced method working

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,7 +1,21 @@
 require_relative './stack.rb'
 
+
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  pairs = {
+    '}' => '{',
+    ']' => '[',
+    ')' => '('
+  }
+  stack = Stack.new
+  string.split("").each do |char|
+    if !pairs[char]
+      stack.push(char)
+    else
+      return false if pairs[char] != stack.pop()
+    end
+  end
+  stack.empty? ? (return true) : (return false)
 end
 
 def evaluate_postfix(postfix_expression)

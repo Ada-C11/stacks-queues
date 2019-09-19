9924aee1a91f4c4a468989def8505c0430f2410b
Karla Guadron <ksguadron@gmail.com>
2019-09-18 20:40:37 -0700

    completed all exercises

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,60 @@
 class Queue
 
+  QUEUE_SIZE = 10
+
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(QUEUE_SIZE)
+    @front = @rear = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
-  end
+    if @front == -1 # Q is empty
+      @rear = 1
+      @front = 0
+      @store[@front] = element
+    elsif @front == @rear
+      raise Error, "Q Full"
+    else # not empty
+      new_rear = (@rear + 1) % QUEUE_SIZE
+      @store[@rear] = element
+      @rear = new_rear
+    end
+  end	 
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 && @rear == -1 # Q is empty
+      raise Error, "Q Empty"
+    elsif @store[@front +1] == nil # Q has one item and becomes empty
+      removed = @store[@front]
+      @store[@front] = nil
+      @front = -1
+      @rear = -1
+      return removed
+    else
+      removed = @store[@front]
+      @store[@front] = nil
+      @front = @front + 1
+      return removed
+    end
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @front
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @store.length
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1 && @rear == -1
+      return true
+    else
+      return false
+    end
   end
 
   def to_s
-    return @store.to_s
+    return @store[@front...@rear].to_s
   end
 end
